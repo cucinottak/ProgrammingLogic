994c725eac9a5f59066287c3f92ec5ed9a72ab20
cucinottak <S01321434@acad.tri-c.edu>
2025-08-04 03:09:46 +0000

    git

---
+++ Lab5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Animal
+{
+    //step 1
+    public virtual void animalSound()
+    {
+        Console.WriteLine("The animal makes a sound");
+    }
+}
+//step 2
+public class Cat : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The cat says: meow");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The dog says: bow wow");
+    }
+}
+
+//step 3
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();
+        Animal myCat = new Cat();
+        Animal myDog = new Dog();
+
+        myAnimal.animalSound();
+        myCat.animalSound();
+        myDog.animalSound();
+    }
+}
